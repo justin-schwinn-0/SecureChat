ced768f22d450b547384d1584e7c85537e43798e
Joshua Nguyen <jnbusi11@gmail.com>
2024-12-07 14:21:18 -0600

    Add openssl header

---
+++ build/CMakeFiles/SecureChat.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SecureChat.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/Server.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/avery/Desktop/NetSec/SecureChat/NetCommon.cpp" "CMakeFiles/Server.dir/NetCommon.cpp.o" "gcc" "CMakeFiles/Server.dir/NetCommon.cpp.o.d"
+  "/home/avery/Desktop/NetSec/SecureChat/NetServer.cpp" "CMakeFiles/Server.dir/NetServer.cpp.o" "gcc" "CMakeFiles/Server.dir/NetServer.cpp.o.d"
+  "/home/avery/Desktop/NetSec/SecureChat/Server.cpp" "CMakeFiles/Server.dir/Server.cpp.o" "gcc" "CMakeFiles/Server.dir/Server.cpp.o.d"
+  "/home/avery/Desktop/NetSec/SecureChat/ServerData.cpp" "CMakeFiles/Server.dir/ServerData.cpp.o" "gcc" "CMakeFiles/Server.dir/ServerData.cpp.o.d"
+  "/home/avery/Desktop/NetSec/SecureChat/Utils.cpp" "CMakeFiles/Server.dir/Utils.cpp.o" "gcc" "CMakeFiles/Server.dir/Utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
